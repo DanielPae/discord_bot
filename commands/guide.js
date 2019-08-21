6adcd756eefce05a0923ea7a5aaf4eb0d98dfa34
unknown <danielpae104@gmail.com>
2019-08-21 13:06:17 -0400

    init commit

---
+++ commands/guide.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'guide',
+	description: 'returns link to the guide',
+	execute(message, args) {
+		message.channel.send('https://drive.google.com/open?id=1CeduGOEbNdSbYW5TCRO-oCQv12QG8Hz23Fq3e0qzhfk');
+	},
+};
---
+++ commands/image.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'image',
+	description: 'for image test',
+	execute(message, args) {
+		console.log(args[0]);
+	},
+};
